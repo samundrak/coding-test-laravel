797d2a67d475bac61fdec84eb9e6bc5e48b0192d
samundrak <samundrak@yahoo.com>
2016-04-10 19:23:27 +0545

    phpunit test case added also build tool gulp is added for watching changes on test cases also to execute phpunit

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,14 @@
-var elixir = require('laravel-elixir');
-
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
-
-elixir(function(mix) {
-    mix.sass('app.scss');
-});
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var shell = require('gulp-shell')
+var notify = require('gulp-notify');
+var plumber =  require('gulp-plumber');
+gulp.task('phpunit', function() {
+    gulp.src('./tests/*.php')
+        .pipe(watch('./tests/*.php'))
+        .pipe(notify('Running Unit tests...'))
+        .on('change',()=>{ console.log('Changes on Test Cases')})
+        .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+        .pipe(shell('phpunit --debug'))
+        .pipe(notify('Test Successfull...'))
+})
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,19 +1,39 @@
 <?php
 
-use Illuminate\Foundation\Testing\WithoutMiddleware;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
+class ExampleTest extends TestCase {
+	/**
+	 * A basic functional test example.
+	 *
+	 * @return void
+	 */
 
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic functional test example.
-     *
-     * @return void
-     */
-    public function testBasicExample()
-    {
-        $this->visit('/')
-             ->see('Laravel 5');
-    }
+	public function testViews() {
+		/*Testing root*/
+		$this->visit('/')
+			->see('add data');
+
+		/*Testing Home views*/
+		$this->visit('/views/partials/home')
+			->see('contact');
+
+		/*Testing List views*/
+		$this->visit('/views/partials/list')
+			->see('phone');
+
+		/*Testing edit views*/
+		$this->visit('/views/partials/edit')
+			->see('gender');
+	}
+
+	public function testApis() {
+		// $response = $this->call('GET', '/api/details/2');
+		// echo $response->getContent();
+		// $this->assertResponseStatus(200);
+
+	}
+
+	public function testControllers() {
+		// $response = $response = $this->action('GET', 'Details@index');
+		echo 1;
+	}
 }
