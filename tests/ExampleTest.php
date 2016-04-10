239fe3cb65d7ae1b0624b8ca1734e00b5ee320e9
samundrak <samundrak@yahoo.com>
2016-04-11 00:12:59 +0545

    travis clean-up set to true and added phpunit

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -9,31 +9,20 @@ class ExampleTest extends TestCase {
 
 	public function testViews() {
 		/*Testing root*/
-		// $this->visit('/')
-		// ->see('add data');
+		$this->visit('/')
+			->see('add data');
 
 		/*Testing Home views*/
-		// $this->visit('/views/partials/home')
-		// ->see('contact');
+		$this->visit('/views/partials/home')
+			->see('contact');
 
 		/*Testing List views*/
-		// $this->visit('/views/partials/list')
-		// ->see('phone');
+		$this->visit('/views/partials/list')
+			->see('phone');
 
 		/*Testing edit views*/
-		// $this->visit('/views/partials/edit')
-		// ->see('gender');
+		$this->visit('/views/partials/edit')
+			->see('gender');
 	}
 
-	public function testApis() {
-		// $response = $this->call('GET', '/api/details/2');
-		// echo $response->getContent();
-		// $this->assertResponseStatus(200);
-
-	}
-
-	public function testControllers() {
-		// $response = $response = $this->action('GET', 'Details@index');
-		echo 1;
-	}
 }
